7a1d0cc2766698064476c98afa1e54e3e3ca00c4
Thomas Volk (MacBook) <info@thomasvolk.de>
2022-12-05 10:14:18 +0100

    day2 part2 improvements

--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -9,14 +9,17 @@ class Day01Test {
     private val inputFile: URL = Day01::class.java.getResource("/Day01-input.txt")!!
     @Test
     fun testDay01Part1() {
-        val elfWithMostCalories = Day01.elfsFromURL(inputFile).sortedByDescending { e -> e.calories() }.first()
-        println("${elfWithMostCalories} calories: ${elfWithMostCalories.calories()}")
+        val elfWithMostCalories = Day01.elfsFromURL(inputFile)
+            .maxByOrNull { e -> e.calories() }!!
+        println("$elfWithMostCalories calories: ${elfWithMostCalories.calories()}")
         assertEquals(74711, elfWithMostCalories.calories())
     }
 
     @Test
     fun testDay01Part2() {
-        val firstThreeElfsWithMostCalories = Day01.elfsFromURL(inputFile).sortedByDescending { e -> e.calories() }.subList(0, 3)
+        val firstThreeElfsWithMostCalories = Day01.elfsFromURL(inputFile)
+            .sortedByDescending { e -> e.calories() }
+            .subList(0, 3)
         println(firstThreeElfsWithMostCalories)
         assertEquals(209481, firstThreeElfsWithMostCalories.map {it.calories()}.sum())
     }
