24eb9204b8153c9f94f5e458ef25ccd80834530b
Thomas Volk (MacBook) <info@thomasvolk.de>
2022-12-06 19:05:12 +0100

    add day04 part 1

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,5 +1,8 @@
 package net.t53k
 
+import java.io.File
+import java.net.URL
+
 object Day04 {
     fun parseLine(line: String): Pair<IntRange, IntRange> {
         val regex = "([0-9]+)\\-([0-9]+),([0-9]+)\\-([0-9]+)".toRegex()
@@ -8,5 +11,24 @@ object Day04 {
         val elfBstart = regex.find(line)!!.groupValues[3].toInt()
         val elfBend = regex.find(line)!!.groupValues[4].toInt()
         return Pair((elfAstart..elfAend), (elfBstart..elfBend))
+    }
+
+    fun sectionContained(a: IntRange, b: IntRange): Boolean {
+        return a.first >= b.first  && a.last <= b.last
+    }
+
+    data class ElfPair(val sectionA: IntRange, val sectionB: IntRange) {
+
+        fun sectionContained(): Boolean {
+           return Day04.sectionContained(sectionA, sectionB) || Day04.sectionContained(sectionB, sectionA)
+        }
+    }
+
+    fun parse(url: URL): List<ElfPair> {
+        File(url.toURI()).useLines { lines ->
+            return lines.toList()
+                .map { parseLine(it) }
+                .map { ElfPair(it.first, it.second) }
+        }
     }
 }
--- src/test/kotlin/Day04Test.kt
+++ src/test/kotlin/Day04Test.kt
@@ -1,8 +1,10 @@
 package net.t53k
 
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 import java.net.URL
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class Day04Test {
     private val inputFile: URL = Day01::class.java.getResource("/Day04-input.txt")!!
@@ -10,5 +12,17 @@ class Day04Test {
     @Test
     fun testParse() {
         assertEquals(Pair((23..34), (1..9)), Day04.parseLine("23-34,1-9"))
+    }
+    @Test
+    fun testElfPair() {
+        assertTrue(Day04.ElfPair((23..34), (23..34)).sectionContained())
+        assertTrue(Day04.ElfPair((4..5), (2..6)).sectionContained())
+        assertTrue(Day04.ElfPair((2..6), (4..5)).sectionContained())
+        assertFalse(Day04.ElfPair((23..34), (22..30)).sectionContained())
+        assertFalse(Day04.ElfPair((10..40), (400..3800)).sectionContained())
+    }
+    @Test
+    fun testDay04() {
+        assertEquals(580, Day04.parse(inputFile).count { it.sectionContained() })
     }
 }
