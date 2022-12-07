1c723bd5d22b158d250f5ecb666211fe7792f7c7
Thomas Volk (MacBook) <info@thomasvolk.de>
2022-12-07 20:56:52 +0100

    tests

--- src/test/kotlin/Day05Test.kt
+++ src/test/kotlin/Day05Test.kt
@@ -2,14 +2,19 @@ package net.t53k
 
 import org.junit.jupiter.api.Test
 import java.net.URL
+import java.util.*
+import kotlin.test.assertEquals
 
 class Day05Test {
     private val inputFile: URL = Day01::class.java.getResource("/Day05-input.txt")!!
 
     @Test
-    fun testParse() {
+    fun testDay05part1() {
         val (cargoStacks, commands) = Day05.parse(inputFile)
         commands.forEach { it.execute(cargoStacks) }
-        println(cargoStacks.map { (i, s) -> s.peek() }.joinToString(""))
+        assertEquals("CFFHVVHNC", peek(cargoStacks))
     }
+
+    private fun peek(cargoStacks: Map<Int, Stack<Char>>) =
+        cargoStacks.map { (i, s) -> s.peek() }.joinToString("")
 }
