dcef495c33b029f8d86854b30553f89fc15f5ba9
Thomas Volk <thomas.volk@trustedshops.com>
2022-12-07 17:30:24 +0100

    day05 parsing

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,4 +1,15 @@
 package net.t53k
 
+import java.io.File
+import java.net.URL
+
 object Day05 {
+    data class CargoMap(val cargoStacks: Map<Int, ArrayDeque<String>>)
+    fun parse(url: URL) {
+        val text = File(url.toURI()).readText()
+        val (map, commands) = text.split("\n\n", limit = 2)
+        println(map.split("\n").last())
+        println("---------------------------")
+        println(commands)
+    }
 }
--- src/test/kotlin/Day05Test.kt
+++ src/test/kotlin/Day05Test.kt
@@ -1,7 +1,13 @@
 package net.t53k
 
+import org.junit.jupiter.api.Test
 import java.net.URL
 
 class Day05Test {
     private val inputFile: URL = Day01::class.java.getResource("/Day05-input.txt")!!
+
+    @Test
+    fun testParse() {
+        Day05.parse(inputFile)
+    }
 }
