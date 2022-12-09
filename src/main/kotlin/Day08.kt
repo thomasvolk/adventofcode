0fe82035a9397a41a30255b07b651686b6349e46
Thomas Volk <thomas.volk@trustedshops.com>
2022-12-09 14:06:31 +0100

    day08 parser

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,4 +1,11 @@
 package net.t53k
 
 object Day08 {
+    data class Forest(val trees: List<List<Int>>) {
+        val colCount = trees.first().count()
+        val rowCount = trees.count()
+    }
+    fun parse(rows: List<String>): Forest {
+        return Forest(rows.map { it.toList().map(Character::getNumericValue)  })
+    }
 }
--- src/test/kotlin/Day08Test.kt
+++ src/test/kotlin/Day08Test.kt
@@ -1,7 +1,25 @@
 package net.t53k
 
+import org.junit.jupiter.api.Test
 import java.net.URL
+import kotlin.test.assertEquals
 
 class Day08Test {
     private val inputFile: URL = Day01::class.java.getResource("/Day08-input.txt")!!
+
+    val testData = """
+        30373
+        25512
+        65332
+        33549
+        35390
+    """.trimIndent()
+
+
+    @Test
+    fun testParser() {
+        val forest = Day08.parse(testData.split("\n"))
+        assertEquals(5, forest.rowCount)
+        assertEquals(5, forest.colCount)
+    }
 }
