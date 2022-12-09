6e4878759144357542258004c4a4139bc1e4ad5a
Thomas Volk (MacBook) <info@thomasvolk.de>
2022-12-10 00:38:00 +0100

    add basic code for Day09

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,4 +1,59 @@
 package net.t53k
 
+import java.io.File
+import java.lang.IllegalArgumentException
+import java.net.URL
+
 object Day09 {
+    data class Position(val x: Int, val y: Int)
+
+    enum class Direction {
+        UP, DOWN, LEFT, RIGHT;
+        companion object {
+            private val mapping = mapOf(
+                "U" to UP,
+                "D" to DOWN,
+                "L" to LEFT,
+                "R" to RIGHT
+                )
+            fun get(shortName: String): Direction {
+                mapping[shortName]?.let { d ->
+                    return d
+                }
+                throw IllegalArgumentException("no direction found for $shortName")
+            }
+        }
+    }
+    class Rope {
+        private var head: Position
+        private var tail: Position
+        private var tailHistory: List<Position> = listOf()
+        constructor(start: Position) {
+            head = start
+            tail = start
+        }
+
+        fun moveHead(direction: Direction, amount: Int) {
+            println("$direction, $amount")
+            TODO("implement")
+        }
+
+        fun execute(moves: List<Pair<Direction, Int>>) {
+            moves.forEach { (d, a) -> moveHead(d, a) }
+        }
+    }
+
+    fun parse(lines: List<String>): List<Pair<Direction, Int>> {
+        return lines
+            .map { it.trim() }
+            .map { it.split(" ") }
+            .map { Pair(Direction.get(it.first()), it.last().toInt()) }
+    }
+    fun parse(url: URL): List<Pair<Direction, Int>> {
+        File(url.toURI()).useLines { lines ->
+            return parse(lines.toList())
+        }
+    }
+
+    fun rope(x: Int, y: Int): Rope = Rope(Position(x, y))
 }
--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -1,7 +1,27 @@
 package net.t53k
 
+import org.junit.jupiter.api.Test
 import java.net.URL
 
 class Day09Test {
     private val inputFile: URL = Day01::class.java.getResource("/Day09-input.txt")!!
+
+    private val testData = """
+        R 4
+        U 4
+        L 3
+        D 1
+        R 4
+        D 1
+        L 5
+        R 2
+    """.trimIndent()
+
+    @Test
+    fun testLogic() {
+        val moves = Day09.parse(testData.split("\n"))
+        val rope = Day09.rope(0,0)
+        rope.execute(moves)
+
+    }
 }
