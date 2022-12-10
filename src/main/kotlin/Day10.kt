66281de1e76dad98ce67517501538e53aae0a026
Thomas Volk (MacBook) <info@thomasvolk.de>
2022-12-10 23:49:16 +0100

    day10 parser

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,4 +1,57 @@
 package net.t53k
 
+import java.io.File
+import java.lang.IllegalArgumentException
+import java.net.URL
+
 object Day10 {
+    data class Register(var x: Int = 1)
+    interface Command {
+        fun execute(register: Register): Boolean
+    }
+
+    class Noop: Command {
+        override fun execute(register: Register): Boolean {
+            return true
+        }
+
+        override fun toString(): String {
+            return "Noop()"
+        }
+
+    }
+
+    class AddX(private val value: Int): Command {
+        private var cyclesLeft = 2
+        override fun execute(register: Register): Boolean {
+            cyclesLeft -= 1
+            val complete =  cyclesLeft < 1
+            if(complete) {
+                register.x += value
+            }
+            return complete
+        }
+
+        override fun toString(): String {
+            return "AddX($value)"
+        }
+
+    }
+
+    fun parse(input: URL) {
+        File(input.toURI()).useLines { lines ->
+            val commands = lines.toList()
+                .map { command(it.trim()) }
+            println(commands)
+        }
+    }
+
+    private fun command(line: String): Command {
+        val parts = line.split(" ")
+        return when(parts[0]) {
+            "noop" -> Noop()
+            "addx" -> AddX(parts[1].toInt())
+            else -> throw IllegalArgumentException("unknown command line: $line")
+        }
+    }
 }
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -1,7 +1,13 @@
 package net.t53k
 
+import org.junit.jupiter.api.Test
 import java.net.URL
 
 class Day10Test {
     private val inputFile: URL = PuzzleInput.loadFile("/Day10-input.txt")
+
+    @Test
+    fun testDay10Part1() {
+        Day10.parse(inputFile)
+    }
 }
