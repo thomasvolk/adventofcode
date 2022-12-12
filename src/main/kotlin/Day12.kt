0848bd9989ea9673346611f10f2780840ccddfe2
Thomas Volk <thomas.volk@trustedshops.com>
2022-12-12 10:45:00 +0100

    day12 parsing

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,11 +1,67 @@
 package net.t53k
 
+import java.lang.IllegalArgumentException
 import java.net.URL
 
 object Day12 {
-    fun stepsForShortestPath(input: String): Long {
-        TODO("implement")
+
+    enum class Direction {
+        EAST, WEST, NORTH, SOUTH
     }
+
+    data class Position(val height: Char) {
+        private var neighbours = mapOf<Direction, Position>()
+        fun addNeighbour(position: Position, direction: Direction) {
+            neighbours = neighbours + (direction to position)
+        }
+        fun neighours() = neighbours.values
+        override fun toString(): String {
+            return "Position(height=$height, neighbours=${neighbours.keys})"
+        }
+    }
+
+    data class Map(val start: Position, val end: Position, val positions: List<Position>){
+        companion object {
+            fun parse(input: String): Map {
+                var start: Position? = null
+                var end: Position? = null
+                val positions = input.split("\n").map { line ->
+                    line.toList().map { c ->
+                        when (c) {
+                            'S' -> {
+                                val s = Position('a')
+                                start = s
+                                s
+                            }
+
+                            'E' -> {
+                                val e = Position('z')
+                                end = e
+                                e
+                            }
+                            else -> Position(c)
+                        }
+                    }
+                }
+                // TODO: assign neighbours
+                start?.let { s ->
+                    end?.let { e ->
+                        return Map(s, e, positions.flatten())
+                    }
+                    throw IllegalArgumentException("input map has no end point 'E' - $input")
+                }
+                throw IllegalArgumentException("input map has no start point 'S' - $input")
+            }
+        }
+    }
+
+    fun stepsForShortestPath(input: String): Long {
+        val map = Map.parse(input)
+
+        println(map)
+        TODO("implement path algorithm")
+    }
+
     fun stepsForShortestPath(input: URL): Long {
         return stepsForShortestPath(input.readText())
     }
