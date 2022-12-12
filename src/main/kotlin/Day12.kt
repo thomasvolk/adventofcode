6e0ae05d2279079c8b3f715896a72fe4640b9101
Thomas Volk (MacBook) <info@thomasvolk.de>
2022-12-12 07:17:14 +0100

    preparation for day12 ~

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,4 +1,12 @@
 package net.t53k
 
+import java.net.URL
+
 object Day12 {
+    fun stepsForShortestPath(input: String): Long {
+        TODO("implement")
+    }
+    fun stepsForShortestPath(input: URL): Long {
+        return stepsForShortestPath(input.readText())
+    }
 }
--- src/test/kotlin/Day12Test.kt
+++ src/test/kotlin/Day12Test.kt
@@ -1,4 +1,18 @@
 package net.t53k
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class Day12Test {
+    @Test
+    fun testSimpleMap() {
+        val inputData = """
+            Sabqponm
+            abcryxxl
+            accszExk
+            acctuvwj
+            abdefghi
+        """.trimIndent()
+        assertEquals(31, Day12.stepsForShortestPath(inputData))
+    }
 }
