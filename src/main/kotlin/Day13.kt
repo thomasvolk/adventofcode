ccddef6556ec5b4d9d77f35552916e30b704ab4e
Thomas Volk <thomas.volk@trustedshops.com>
2022-12-14 20:00:47 +0100

    day13

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -1,4 +1,33 @@
 package net.t53k
 
+import java.net.URL
+import javax.script.ScriptEngineManager
+
 object Day13 {
+    interface Packet {
+    }
+    data class ListPacket(val values: List<Packet>): Packet
+    data class NumberPacket(val value: Int): Packet
+    data class PacketPair(val index: Int, val first: Packet, val second: Packet) {
+        companion object {
+            val jsEngineFactory = ScriptEngineManager()
+            val jsEngine = jsEngineFactory.getEngineByName("javascript")
+            fun parse(pair: String): PacketPair {
+                val (first, last) = pair.split("\n").map { jsEngine.eval(it) }
+                println("first: $first")
+                println("last: $last")
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    fun parse(input: URL): List<PacketPair> {
+        return parse(input.readText())
+    }
+
+    fun parse(input: String): List<PacketPair> {
+        return input.split("\n\n").map {pair ->
+            PacketPair.parse(pair)
+        }
+    }
 }
--- src/test/kotlin/Day13Test.kt
+++ src/test/kotlin/Day13Test.kt
@@ -1,7 +1,38 @@
 package net.t53k
 
+import org.junit.jupiter.api.Test
 import java.net.URL
 
 class Day13Test {
     private val inputFile: URL = PuzzleInput.loadFile("/Day13-input.txt")
+
+    @Test
+    fun testExample() {
+        val testData = """
+            [1,1,3,1,1]
+            [1,1,5,1,1]
+
+            [[1],[2,3,4]]
+            [[1],4]
+
+            [9]
+            [[8,7,6]]
+
+            [[4,4],4,4]
+            [[4,4],4,4,4]
+
+            [7,7,7,7]
+            [7,7,7]
+
+            []
+            [3]
+
+            [[[]]]
+            [[]]
+
+            [1,[2,[3,[4,[5,6,7]]]],8,9]
+            [1,[2,[3,[4,[5,6,0]]]],8,9]
+        """.trimIndent()
+        val packetPairs = Day13.parse(testData)
+    }
 }
