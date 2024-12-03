bb36befdb9658fbfbbd92658d78b469f9ca47651
Thomas Volk (MacBook) <info@thomasvolk.de>
2024-12-03 21:49:01 +0100

    day03 b

--- 2024/lib/day03.ml
+++ 2024/lib/day03.ml
@@ -1,17 +1,35 @@
 open Re
 
-let get_multiplication_summary src = 
+let calculate_summary data =  
   let re_mul = Perl.compile_pat "mul\\((\\d+)\\,(\\d+)\\)" in
-  let data = Io.Resource.read_all src in
   Re.all re_mul data 
     |> List.map (fun g -> ((Re.Group.get g 1), (Re.Group.get g 2)))
     |> List.map (fun (a, b) -> ((int_of_string a), (int_of_string b)))
     |> List.map (fun (a, b) -> a * b)
     |> List.fold_left (+) 0
 
-let get_active_fragments src =
+let get_multiplication_summary src = 
+  let data = Io.Resource.read_all src in
+  calculate_summary data
+
+let get_part_after_first_occurrence pattern s =
+  match Re.exec_opt pattern s with
+  | Some groups ->
+      let start_pos = Group.start groups 0 in
+      Some (String.sub s start_pos ((String.length s) - start_pos))
+  | None -> None
+
+let get_multiplication_summary_from_active_fragments src =
   let re_do = Perl.compile_pat "do\\(\\)" in
   let re_dont = Perl.compile_pat "don\\'t\\(\\)" in
   let data = "do()" ^ (Io.Resource.read_all src) in
   Re.split re_dont data 
+   |> List.map (get_part_after_first_occurrence re_do)
+   |> List.map (fun p ->
+       match p with
+         | None -> 0
+         | Some d -> calculate_summary d
+    )
+   |> List.fold_left (+) 0
+
 
--- 2024/test/test_day03.ml
+++ 2024/test/test_day03.ml
@@ -7,6 +7,10 @@ let tests =
       assert_equal ~printer:string_of_int 161 (Day03.get_multiplication_summary "day03-sample");
       assert_equal ~printer:string_of_int 187825547 (Day03.get_multiplication_summary "day03")
     );
+    "b" >:: (fun _ -> 
+      assert_equal ~printer:string_of_int 48 (Day03.get_multiplication_summary_from_active_fragments "day03-b-sample");
+      assert_equal ~printer:string_of_int 85508223 (Day03.get_multiplication_summary_from_active_fragments "day03")
+    );
   ]
 
   let _ = run_test_tt_main tests
