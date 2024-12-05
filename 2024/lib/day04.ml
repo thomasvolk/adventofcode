68ef6562def3854a7e7b99cbb2788eca9573843d
Thomas Volk <thomas.volk@trustedshops.de>
2024-12-05 10:24:39 +0100

    day04 alg

--- 2024/lib/day04.ml
+++ 2024/lib/day04.ml
@@ -20,29 +20,42 @@ let get x y matrix =
       | None -> None
       | v -> v
 
-let collection d x y matrix =
+let collect d x y matrix =
   let (w, _h) = dimensions matrix in
-  let rec collect a n get_pos =
+  let rec collect_loop a n get_pos =
     let (x, y) = get_pos n in
     match get x y matrix with
       | None -> a
-      | Some v -> collect (a ^ v) (n + 1) get_pos
+      | Some v -> collect_loop (a ^ v) (n + 1) get_pos
   in
   match d with
-    | Horizontal -> collect "" 0 (fun n -> (x + n, y))
-    | Vertical -> collect "" 0 (fun n -> (x, y + (w * n)))
-    | TopLeftBottomRight -> collect "" 0 (fun n -> (x + n , y + w))
-    | TopRightBottomLeft -> collect "" 0 (fun n -> (x - n , y + w))
+    | Horizontal -> collect_loop "" 0 (fun n -> (x + n, y))
+    | Vertical -> collect_loop "" 0 (fun n -> (x, y + (w * n)))
+    | TopLeftBottomRight -> collect_loop "" 0 (fun n -> (x + n , y + w))
+    | TopRightBottomLeft -> collect_loop "" 0 (fun n -> (x - n , y + w))
 
-let xmas_count src =
-  let matrix = read_matrix src in
+let re_xmas = Perl.compile_pat "XMAS"
+let re_samx  = Perl.compile_pat "SAMX"
+
+let count s = 
+  let xmas_cnt = Re.all re_xmas s |> List.length in
+  let samx_cnt = Re.all re_samx s |> List.length in
+  xmas_cnt + samx_cnt
+
+let collect_and_count d x y matrix = collect d x y matrix |> count
+
+let count_trough_all_paths matrix =
   let (w, h) = dimensions matrix in
-  let re_xmas = Perl.compile_pat "XMAS" in
-  let re_samx  = Perl.compile_pat "SAMX" in
-  let count s = 
-    let xmas_cnt = Re.all re_xmas s |> List.length in
-    let samx_cnt = Re.all re_samx s |> List.length in
-    xmas_cnt + samx_cnt
+  let rec walk_down ?(stop = 0) c i f =
+    match i with
+     | i when i < stop -> c
+     | _ -> walk_down ~stop:stop (c + (f i)) (i - 1) f
   in
-  Error "not implemented"
+  let cnt_rows = walk_down 0 (h - 1) (fun y -> collect_and_count Vertical 0 y matrix) in
+  let cnt_cols = walk_down 0 (w - 1) (fun x -> collect_and_count Horizontal x 0 matrix) in
+  let cnt_tlbr_y = walk_down ~stop:1 0 (h - 1) (fun y -> collect_and_count TopLeftBottomRight 0 y matrix) in
+  let cnt_tlbr_x = walk_down 0 (w - 1) (fun x -> collect_and_count TopLeftBottomRight x 0 matrix) in
+  let cnt_trbl_y = walk_down ~stop:1 0 (h - 1) (fun y -> collect_and_count TopRightBottomLeft 0 y matrix) in
+  let cnt_trbl_x = walk_down 0 (w - 1) (fun x -> collect_and_count TopRightBottomLeft x 0 matrix) in
+  cnt_cols + cnt_rows + cnt_tlbr_x + cnt_tlbr_y + cnt_trbl_x + cnt_trbl_y
 
--- 2024/test/test_day04.ml
+++ 2024/test/test_day04.ml
@@ -1,6 +1,5 @@
 open OUnit2
 open Aoc2024
-open Day04
 
 let tests =
   "Day04" >::: [
@@ -8,7 +7,7 @@ let tests =
       let m = Day04.read_matrix "day04" in
       assert_equal ~printer:string_of_int 140 (fst (Day04.dimensions m));
       assert_equal ~printer:string_of_int 140 (snd (Day04.dimensions m));
-      assert_equal ~printer:Fun.id "" (Day04.collection Horizontal 0 0 m);
+      assert_equal ~printer:string_of_int 18 (Day04.count_trough_all_paths (Day04.read_matrix "day04-sample"))
     );
   ]
 
