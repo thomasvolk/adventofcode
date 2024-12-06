bf5bc231263d5468f816e11f1ff8cf9862472c32
Thomas Volk <thomas.volk@trustedshops.de>
2024-12-06 12:19:35 +0100

    day04 third trial ..

--- 2024/lib/day04.ml
+++ 2024/lib/day04.ml
@@ -11,57 +11,71 @@ end
 
 module Matrix = struct
 
-  type t = int list list
+  type t = { 
+    width: int;
+    height: int;
+    values: string list;
+  }
 
   let explode_string s = List.init (String.length s) (fun i -> String.get s i |> Char.escaped)
 
-  let read src =
-    Io.Resource.read_lines src
-    |> List.map explode_string
-
-  let get p m = 
-    match Point.positive p with
-      | false -> None
-      | true -> match List.nth_opt m (Point.y p) with
-        | None -> None
-        | Some r -> match List.nth_opt r (Point.x p) with
-          | None -> None
-          | v -> v
-
-  let dimensions m = 
+  let create src =
+    let m = Io.Resource.read_lines src
+    |> List.map explode_string in
     let w = List.length (List.nth m 0) in
     let h = List.length m in 
-    (w, h)
+    {
+      width = w;
+      height = h;
+      values = m |> List.flatten;
+    }
 
+  let size t = (t.width * t.height) 
+
+  let position p t = match (t.width * (Point.y p)) + (Point.x p) with 
+    | i when i < 0 -> None
+    | i when i >= size t -> None
+    | i -> Some i
+
+  let coordinates i t = Point.create (i mod t.width) (i / t.width) 
+
+  let get p t = 
+    match position p t with
+      | None -> None
+      | Some i -> List.nth_opt t.values i
+
+  let map f t = List.mapi (fun i v -> f (coordinates i t) v) t.values
 end
 
-let value = function
-  | "XMAS" | "SAMX" -> 1
-  | _ -> 0
+module Word = struct
+  include String
+  let of_points l = l |> List.map Point.to_string |> String.concat "-"
+end
 
-let words p m =
-  let rec walk_loop a step p dp =
+module WordRegister = Set.Make(Word)
+
+let has_xmas_match = function
+  | "XMAS" | "SAMX" -> true
+  | _ -> false
+
+let words start m =
+  let rec walk_loop coordinates word step p dp =
     match Matrix.get p m with
-      | None -> a
-      | Some _ when step <= 0 -> a
-      | Some v -> walk_loop (a ^ v) (step - 1) (Point.add p dp) dp
+      | Some v when step > 0 -> walk_loop (coordinates @ [p]) (word ^ v) (step - 1) (Point.add p dp) dp
+      | _ -> (word, coordinates)
   in
-  let walk = walk_loop "" 4 in
+  let walk = walk_loop [] "" 4 in
   [(0, -1); (1, -1); (1, 0); (1, 1); (0, 1); (-1, 1); (-1, 0); (-1, -1)]
-      |> List.map (fun dp -> walk p dp)
+      |> List.map (walk start)
 
-let count p m = 
-  words p m |> List.map value |> List.fold_left (+) 0
+let count_all src = 
+  let m = Matrix.create src in
+  Matrix.map (fun p _ -> words p m) m
+   |> List.flatten
+   |> List.filter (fun (w, _) -> has_xmas_match w)
+   |> List.map (fun (_, c) -> Word.of_points c)
+   |> WordRegister.of_list
+   |> WordRegister.to_list
+   |> List.length
 
-let count_all src =
-  let m = Matrix.read src in
-  let (w, h) = Matrix.dimensions m in
-  let cols v r = function
-    | c when c < 0 -> v
-    | c -> v + (count (Point.create (c - 1) r) m)
-  in
-  let rows v = function
-    | r when r < 0 -> v
-    | r -> cols v (r - 1) w
-  in
-  rows 0 h
+
