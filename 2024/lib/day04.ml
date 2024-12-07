7b64cc326a1a555cfe891b2ece1963000d61b0c2
Thomas Volk (MacBook) <info@thomasvolk.de>
2024-12-07 13:18:25 +0100

    day04 faster ... but still wrong :-(

--- 2024/lib/day04.ml
+++ 2024/lib/day04.ml
@@ -1,4 +1,8 @@
 
+let rec range i j = 
+  if i > j then [] 
+  else i :: range (i+1) j
+
 module Point = struct
   type t = { x: int; y: int }
   let create x y = { x = x; y = y }
@@ -6,6 +10,7 @@ module Point = struct
   let y t = t.y
   let move (x, y) a = create (a.x + x) (a.y + y)
   let to_string t = "P(" ^ (string_of_int t.x) ^ "," ^ (string_of_int t.y) ^ ")"
+  let inside w h t = t.x < w && t.y < h && t.x >= 0 && t.y >= 0
   let rotate w h t = create (t.x mod w) (t.y mod h)
 end
 
@@ -14,7 +19,7 @@ module Matrix = struct
   type t = { 
     width: int;
     height: int;
-    values: string list;
+    data: string list list;
   }
 
   let explode_string s = List.init (String.length s) (fun i -> String.get s i |> Char.escaped)
@@ -27,60 +32,50 @@ module Matrix = struct
     {
       width = w;
       height = h;
-      values = m |> List.flatten;
+      data = m;
     }
 
   let size t = (t.width * t.height) 
 
-  let position p t = match (t.width * (Point.y p)) + (Point.x p) with 
-    | i when i < 0 -> None
-    | i when i >= size t -> None
-    | i -> Some i
-
   let coordinates i t = Point.create (i mod t.width) (i / t.width) 
 
   let get p t = 
-    match position p t with
-      | None -> None
-      | Some i -> List.nth_opt t.values i
+    match Point.inside t.width t.height p with
+      | false -> None
+      | true -> let row = List.nth t.data p.y in
+                List.nth_opt row p.x
 
-  let paths start t =
-    let rec walk_loop coordinates word step p dp =
+  let path start vec t =
+    let rec walk_loop path p dp =
       match get p t with
-        | Some v when step > 0 -> walk_loop (coordinates @ [p]) (word ^ v) (step - 1) (Point.move dp p) dp
-        | _ -> (word, coordinates)
+        | Some v when Point.inside t.width t.height p -> walk_loop (path ^ v) (Point.move dp p) dp
+        | _ -> path
     in
-    let walk = walk_loop [] "" 4 in
-    [(0, -1); (1, -1); (1, 0); (1, 1); (0, 1); (-1, 1); (-1, 0); (-1, -1)]
-        |> List.map (walk start)
+    walk_loop "" start vec
 
-  let walk is_start t = t.values
-    |> List.mapi (fun i v ->
-        match is_start v with
-          | true -> Some (paths (coordinates i t) t)
-          | false -> None
-      )
-    |> List.filter Option.is_some
-    |> List.map Option.get
+  let col x = path (Point.create x 0) (0, 1)
+
+  let cols t = range 0 t.width |> List.map (fun x -> col x t)
+
+  let rows t = t.data |> List.map (String.concat "") 
+
+  let dia_tl_br t = 
+      (range 1 (t.height - 1) |> List.map (fun y -> path (Point.create 0 y) (1 ,1) t))
+    @ (range 0 (t.width - 1) |> List.map (fun x -> path (Point.create x 0) (1, 1) t))
+
+  let dia_tr_bl t = 
+      (range 1 (t.height - 1) |> List.map (fun y -> path (Point.create 0 y) (1 ,-1) t))
+    @ (range 0 (t.width - 1)  |> List.map (fun x -> path (Point.create x (t.height - 1)) (1, -1) t))
+
 end
 
-let string_of_coordinates l = l |> List.map Point.to_string |> List.sort compare |> String.concat "-"
+let re_xmas = Re.Perl.compile_pat "XMAS"
+let re_samx = Re.Perl.compile_pat "SAMX"
 
-let has_xmas_match = function
-  | "XMAS" | "SAMX" -> true
-  | _ -> false
-
-let is_start = function
-  | "X" | "S" -> true
-  | _ -> false
+let count s = (Re.all re_xmas s |> List.length) + (Re.all re_samx s |> List.length)
 
 let count_all src = 
-  Matrix.create src
-   |> Matrix.walk is_start
-   |> List.flatten
-   |> List.filter (fun (w, _) -> has_xmas_match w)
-   |> List.map (fun (_, c) -> string_of_coordinates c)
-   |> List.sort_uniq compare
-   |> List.length
-
-
+  let m = Matrix.create src in
+  (Matrix.rows m) @ (Matrix.cols m) @ (Matrix.dia_tl_br m) @ (Matrix.dia_tr_bl m)
+    |> List.map count
+    |> List.fold_left (+) 0
--- 2024/test/test_day04.ml
+++ 2024/test/test_day04.ml
@@ -4,12 +4,13 @@ open Aoc2024
 let tests =
   "Day04" >::: [
     "a" >:: (fun _ -> 
+      assert_equal ~printer:string_of_int 3 (Day04.count "XMASSSSAXSAMXMASXXX");
       assert_equal ~printer:string_of_int 18 (Day04.count_all "day04-sample");
       let open Day04.Matrix in
       let m = create "day04" in
       assert_equal ~printer:string_of_int 140 m.width;
       assert_equal ~printer:string_of_int 140 m.height;
-      assert_equal ~printer:string_of_int 2496 (Day04.count_all "day04");
+      assert_equal ~printer:string_of_int 2473 (Day04.count_all "day04");
     );
   ]
 
