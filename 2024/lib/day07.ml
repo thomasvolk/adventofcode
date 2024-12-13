6b86b2a7cb21ecee1570a66e9840eb9cae20a354
Thomas Volk (MacBook) <info@thomasvolk.de>
2024-12-13 11:06:24 +0100

    day07 first simple check

--- 2024/lib/day07.ml
+++ 2024/lib/day07.ml
@@ -15,10 +15,31 @@ module Equation = struct
      | t::num -> Some { test = t; numbers = num }
      | [] -> None
 
+  let test e = e.test
+
   let load src =
     Io.Resource.read_lines src
       |> List.map parse
       |> List.filter Option.is_some
       |> List.map Option.get
 
+  let to_string e = "Equation(test=" ^
+                              (string_of_int e.test) ^ 
+                            " numbers=" ^ 
+                              (e.numbers |> List.map string_of_int |> String.concat ",") ^ ")"
+
+  let is_valid e = 
+    let sum = e.numbers |> List.fold_left (+) 0 in
+    let prd = e.numbers |> List.fold_left (fun a b -> a * b) 1 in
+    if sum > e.test || prd < e.test
+    then 
+      false
+    else
+      true
 end
+
+let sum_all_valid_equations src =
+  Equation.load src
+    |> List.filter Equation.is_valid
+    |> List.map Equation.test
+    |> List.fold_left (+) 0
--- 2024/test/test_day07.ml
+++ 2024/test/test_day07.ml
@@ -1,9 +1,11 @@
 open OUnit2
+open Aoc2024
 
 let tests =
   "Day07" >::: [
     "a" >:: (fun _ -> 
-      assert_equal ~printer:string_of_int 0 0;
+      assert_equal ~printer:string_of_int 9 (List.length (Day07.Equation.load "day07-sample"));
+      assert_equal ~printer:string_of_int 3749 (Day07.sum_all_valid_equations "day07-sample");
     );
     "b" >:: (fun _ -> 
       assert_equal ~printer:string_of_int 0 0;
