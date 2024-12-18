a47c0dde796db32c3654b721f63064695d626633
Thomas Volk (MBP-von-Thomas.fritz.box) <info@thomasvolk.de>
2024-12-18 18:35:05 +0100

    day07 recursive

--- 2024/lib/day07.ml
+++ 2024/lib/day07.ml
@@ -7,37 +7,8 @@ let int_log10 a = Float.log10 (float_of_int a) |> int_of_float
 let concat a b = (string_of_int a) ^ (string_of_int b) |> int_of_string
 *)
 let concat a b = b + (a * (10 * ((int_log10 b) +1))) 
+let (--) = concat
 
-let bits ?(base=2) s n =
-  let rec b_loop bits p n =
-    match p with
-    | 0 -> bits
-    | p ->
-        let bit = base ^^ (p - 1) in 
-        let rec value d = 
-          let r = n - (bit * d) in
-          match r with
-           | r when r >= 0 -> d
-           | r when r < 0 && d > 0 -> value (d - 1)
-           | _ -> 0
-        in
-        let v = value (base - 1) in
-        let result = bits @ [v] in
-        let nn = n - (bit * v) in
-        b_loop result (p - 1) nn
-  in
-  let an = Int.abs n in
-  b_loop [] s an
-
-
-let zip_bits ~transform:tranform al bl =
-  let rec tr_loop r = function
-    | ([], _) -> r
-    | (_, []) -> r
-    | (a :: atl, b :: btl) -> tr_loop (r @ [(tranform a b)]) (atl, btl)
-  in
-  tr_loop [] (al, bl)
-    
 
 module Equation = struct
   type t = { test: int; numbers: int list }
@@ -68,66 +39,19 @@ module Equation = struct
                             " numbers=" ^ 
                               (e.numbers |> List.map string_of_int |> String.concat ",") ^ ")"
 
-  let to_operator = function
-    | 0 -> ( * )
-    | 1 -> ( + )
-    | _ -> concat
 
-  let bits_map ?(base = 2) cnt = 
-    let rec op_loop r = function
-     | 0 -> r
-     | v -> op_loop (r @ [(bits ~base:base cnt (v - 1))]) (v - 1)
+  let is_valid ?(concat = false) e =
+    let rec is_valid_loop c = function
+      | [] -> c = e.test
+      | n :: tl -> let r = 
+          (is_valid_loop (c * n) tl) ||
+          (is_valid_loop (c + n) tl)
+          in
+          if concat then r || is_valid_loop (c -- n) tl else r
     in
-    op_loop [] (base ^^ cnt)
-
-  let calculate bits numbers =
-    let rec calculate_loop x bl nl = 
-      match (bl, nl) with
-        | ([], []) -> x
-        | ((b :: btl), (y :: ntl)) -> calculate_loop ((to_operator b) x y) btl ntl
-        | _ -> 0
-    in
-    match numbers with
-     | a :: ntl -> calculate_loop a bits ntl
-     | [] -> 0
-
-  let possible_results ?(base = 2) e =
-    (* we have one operator less then the count of numbers *)
-    let op_cnt = (List.length e.numbers) - 1 in
-    let bm = bits_map ~base:base op_cnt in
-    bm |> List.map (fun o -> (calculate o e.numbers, o))
-
-  let is_valid ?(base = 2) e =
-    possible_results ~base:base e 
-      |> List.map (fun (r, _) -> r)
-      |> List.exists ((=) e.test)
-
-  let is_valid_with_concat e =
-    let results =
-      possible_results ~base:2 e 
-      |> List.map (fun (r, _) -> r)
-    in
-    if List.exists ((=) e.test) results
-    then
-      true
-    else
-      let op_cnt = (List.length e.numbers) - 1 in
-      let cbm = bits_map ~base:2 op_cnt in
-      let transform = zip_bits ~transform:(fun a b -> if b = 1 then 2 else a) in
-      let rec revalidate o = function
-        | [] -> false
-        | bm :: tl -> 
-            let r = calculate (transform o bm) e.numbers in
-            if r = e.test then
-              true
-            else
-              revalidate o tl
-      in
-      let lower_results =
-        possible_results ~base:2 e 
-        |> List.filter (fun (r, _) -> e.test > r)
-      in
-      lower_results |> List.exists (fun (_, o) -> revalidate o cbm)
+    match e.numbers with
+      | [] -> false
+      | s :: r -> is_valid_loop s r
 end
 
 let sum_all_valid_equations src =
@@ -138,6 +62,6 @@ let sum_all_valid_equations src =
 
 let sum_all_valid_equations_with_concat src =
   Equation.load src
-    |> List.filter Equation.is_valid_with_concat
+    |> List.filter (Equation.is_valid ~concat:true)
     |> List.map Equation.test
     |> List.fold_left (+) 0
--- 2024/test/test_day07.ml
+++ 2024/test/test_day07.ml
@@ -1,31 +1,15 @@
 open OUnit2
 open Aoc2024
 
-let string_of_int_list l = l |> List.map string_of_int |> String.concat ", "
-
 let tests =
   "Day07" >::: [
     "a" >:: (fun _ -> 
       assert_equal ~printer:string_of_int 9 (List.length (Day07.Equation.load "day07-sample"));
-      assert_equal ~printer:string_of_int_list [1; 1; 1] (Day07.bits 3 7);
-      assert_equal ~printer:string_of_int_list [1; 0; 1] (Day07.bits 3 5);
-      assert_equal ~printer:string_of_int_list [1; 0; 0] (Day07.bits 3 4);
-      assert_equal ~printer:string_of_int_list [1] (Day07.bits 1 1);
-      assert_equal ~printer:string_of_int_list [0] (Day07.bits 1 0);
-      assert_equal ~printer:string_of_int_list [1] (Day07.bits 1 (-1));
-
-      assert_equal [[1]; [0]] (Day07.Equation.bits_map 1);
 
       assert_equal ~printer:string_of_int 3749 (Day07.sum_all_valid_equations "day07-sample");
       assert_equal ~printer:string_of_int 12839601725877 (Day07.sum_all_valid_equations "day07");
     );
     "b" >:: (fun _ -> 
-      assert_equal ~printer:string_of_int_list [1; 1] (Day07.bits ~base:3 2 4);
-      assert_equal ~printer:string_of_int_list [1; 2] (Day07.bits ~base:3 2 5);
-      assert_equal ~printer:string_of_int_list [2; 0] (Day07.bits ~base:3 2 6);
-      assert_equal ~printer:string_of_int_list [2; 1] (Day07.bits ~base:3 2 7);
-      assert_equal ~printer:string_of_int_list [2; 2] (Day07.bits ~base:3 2 8);
-
       assert_equal ~printer:string_of_int 11387 (Day07.sum_all_valid_equations_with_concat "day07-sample");
       assert_equal ~printer:string_of_int 0 (Day07.sum_all_valid_equations_with_concat "day07");
       (*
